d4462fa06bc707d8d3f4c03a682a1b5aaea288f5
makad3382 <45436557+makad3382@users.noreply.github.com>
2018-11-28 15:08:28 -0500

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -11,12 +11,39 @@ package sdlc;
  * @author sadyk1876
  */
 public class Notes extends javax.swing.JFrame {
-
     /**
      * Creates new form Notes
      */
     public Notes() {
         initComponents();
+        
+        //Declare sections attribute
+        private String[][] sections;
+    
+        //Constructor one
+        //Set sections to null
+        public Notes(){
+            sections = null;
+        }
+    
+        //Constructor 2
+        //Set sections to 2D parameter "s"
+        public Notes(String[][] s){
+            sections = s;
+        }
+    
+        //Getter
+        //Returns the note at the passed index
+        public String getNote(int index){
+            return sections[index][1];
+        }
+    
+        //Setter
+        //Sets the title and note of the the passed index
+        public void setNote(int index, String title, String note){
+            sections[index][0] = title;
+            sections[index][1] = note;
+        }
     }
 
     
