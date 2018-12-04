77b91441c2e86e40f4431e2376da7b4ac777075e
makad3382 <45436557+makad3382@users.noreply.github.com>
2018-12-04 14:31:41 -0500

    Add files via upload

---
+++ Notes.java
@@ -0,0 +1,41 @@
+/*
+ *  Masfia Kader, Sarah Dykstra
+    November 28, 2018   
+    PM Project "Notes" class.
+ */
+
+public class Notes {
+    //Declare sections attribute
+    private String[][] sections;
+    
+    //Constructor one
+    //Set sections to null
+    public Notes(){
+        sections = null;
+    }
+    
+    //Constructor 2
+    //Set sections to 2D parameter "s"
+    public Notes(String[][] s){
+        sections = s;
+    }
+    
+    //Getter
+    //Returns the note at the passed index
+    public String getNote(int index){
+        return sections[index][1];
+    }
+    
+    //Returns title at the passed index
+    public String getTitle(int index){
+        return sections[index][0];
+    }
+    
+    //Setter
+    //Sets the title and note of the the passed index
+    public void setNote(int index, String title, String note){
+        sections[index][0] = title;
+        sections[index][1] = note;
+    }
+    
+}
